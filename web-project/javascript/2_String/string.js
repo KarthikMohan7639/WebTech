b8febcaa055f7c9b1eba8971e0163001c6b70306
karthikmohan7639@gmail.com <karthikmohan7639@gmail.com>
2025-07-10 12:31:47 +0530

    Add string manipulation methods and examples in string.js

--- web-project/javascript/2_String/string.js
+++ web-project/javascript/2_String/string.js
@@ -59,3 +59,100 @@ console.log(addresse.charAt(2))
 // if it is present it will return true otherwise it will return false.
 
 console.log(addresse.includes('n'))
+
+// trim()
+//it is used to remove trailing and leading whitespaces from the string.
+let mobileName=' samsung'
+
+console.log(mobileName)
+console.log(mobileName.length);
+
+let mobileName2=mobileName.trim();
+
+console.log(mobileName2);
+console.log(mobileName2.length);
+
+console.log(mobileName)
+
+//split()
+//it is used to split the string into multiple characters or string array based on arguments passed to the function
+
+let wish='good morning everyone'
+
+let arr=wish.split(" ")  // here only one element will be present
+
+console.log(wish.split()) // here only one element will be present
+console.log(wish.split(' '))// splits the string based on spaces
+
+console.log(arr)
+
+
+// ! replace()
+//it will replace the passed string with new string
+
+let information="I'm from bangalore";
+let replacedString=information.replace("bangalore","chennai")
+
+console.log(replacedString);
+
+//replaceAll()
+//it will replace all ocrrusrances of the given string
+
+let replce2=information.replaceAll("a","@")
+console.log(replce2)
+
+//concat()
+//it is used to concatenate (add or combine) two strings
+
+let str4='good afternoon'
+let usernam='virat'
+
+let newStr=str4.concat(" ",usernam,' how are you?')
+console.log(newStr)
+
+// slice()
+// it is used ot extract one string from another string.
+//it will two parameters(startIndex, endIndex)
+//it will not include the endIndex value.
+
+let str5="hello how are you"
+console.log(str5.slice(1,4))
+
+console.log(str5.slice(6));
+
+console.log(str5.slice(-7,0)) //no output
+
+console.log(str5.slice(-5,0))
+
+console.log(str5.slice(-3,-1))
+
+//substring()
+
+// substring method is used extract the new string from the original string 
+// it has two parameters startIndex and endIndex
+//it doesn't take negative parameters
+
+ console.log(str5.substring(1,4));//ell
+
+ console.log(str5.substring(6));
+
+ console.log(str5.substring(-7));
+
+ console.log(str5.substring(4,1))
+
+// String Interpolation
+
+let num1=10
+let num2=20
+
+let add=num1+num2
+
+console.log(`the addition of ${num1} and ${num2} is ${add}`);
+
+let num3=20
+let num4=10
+console.log(`the  subtraction of ${num3} and ${num4} is ${20-10} `)
+ 
+ 
+ 
+
